2176945307ec564d3fd13f4346621dd1e22b1f04
Jonathan Ansley <ansley.jonathan.e@gmail.com>
2017-08-11 19:39:04 -0400

    server listening

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+
+	{
+		name: "Grimace"
+		photo: "http://cdn1-www.craveonline.com/assets/mandatory/legacy/2016/07/man_file_1111706_mcdonald27sorigins3.jpg"
+		scores: [
+			"2",
+			"3",
+			"5",
+			"1",
+			"4",
+			"5",
+			"3",
+			"1",
+			"4",
+			"2"
+		]	
+
+	},
+
+	{
+		name: "Grimace"
+		photo: "http://cdn1-www.craveonline.com/assets/mandatory/legacy/2016/07/man_file_1111706_mcdonald27sorigins3.jpg"
+		scores: [
+			"2",
+			"3",
+			"5",
+			"1",
+			"4",
+			"5",
+			"3",
+			"1",
+			"4",
+			"2"
+		]	
+
+	},
+
+
+	{
+		name: "Grimace"
+		photo: "http://cdn1-www.craveonline.com/assets/mandatory/legacy/2016/07/man_file_1111706_mcdonald27sorigins3.jpg"
+		scores: [
+			"2",
+			"3",
+			"5",
+			"1",
+			"4",
+			"5",
+			"3",
+			"1",
+			"4",
+			"2"
+		]	
+
+	},
+
+
+	{
+		name: "Grimace"
+		photo: "http://cdn1-www.craveonline.com/assets/mandatory/legacy/2016/07/man_file_1111706_mcdonald27sorigins3.jpg"
+		scores: [
+			"2",
+			"3",
+			"5",
+			"1",
+			"4",
+			"5",
+			"3",
+			"1",
+			"4",
+			"2"
+		]	
+
+	},
+
+
+	{
+		name: "Grimace"
+		photo: "http://cdn1-www.craveonline.com/assets/mandatory/legacy/2016/07/man_file_1111706_mcdonald27sorigins3.jpg"
+		scores: [
+			"2",
+			"3",
+			"5",
+			"1",
+			"4",
+			"5",
+			"3",
+			"1",
+			"4",
+			"2"
+		]	
+
+	},
+
+
+
+]
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+let app = express();
+const bodyParser = require('body-parser')
+
+let PORT = process.env.PORT || 3000;
+
+// Create application / JSON parser
+var jsonParser = bodyParser.json()
+
+// const path = require('path')
+
+// Create application / x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false})
+
+// let tableData = require("./data/tables.js")
+// let waitListData = require("./data/waitList.js")
+
+
+// app.use(bodyParser.json());
+
+// Parse and HTML body into a string.
+app.use(bodyParser.text({type: 'text/html'}));
+
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+require("./app/routing/htmlroutes.js")(app);
+
+// app.use(express.static('public'))
+
+// Front Page
+// app.get("/", (req, res)=>{
+//   res.sendFile(path.join(__dirname, "/public/home.html"))
+// })
+
+// Tables
+// app.get("/tables", (req, res)=>{
+//   res.sendFile(path.join(__dirname, "/public/tables.html"))
+// })
+
+// Reservation
+// app.get("/reserve", (req, res)=>{
+//   res.sendFile(path.join(__dirname, "/public/reserve.html"))
+// })
+
+// TABLES JSON
+// app.post("/api/tables", (req, res)=>{
+//   if (tableData.length < 5){
+//     tableData.push(req.body)
+//     res.json(true);
+//   } else {
+//     waitListData.push(req.body)
+//     res.json(false);
+//   }
+
+// })
+
+// app.get("/api/tables", (req, res)=>{
+//   res.json(tableData)
+// })
+
+// WAITLIST JSON
+// app.get("/api/waitlist", (req, res)=>{
+//   res.json(waitListData)
+// })
+
+
+app.listen(PORT, ()=>{
+  console.log("Server started on PORT: ", PORT);
+})
